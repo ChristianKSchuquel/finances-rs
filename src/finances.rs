69530e3f718f3904ec4507dd4b4e4d08531e3b30
Christian Schuquel <christianks@protonmail.com>
2023-04-07 18:32:02 -0300

    Implemented "Add" funtion

--- src/finances.rs
+++ src/finances.rs
@@ -1,8 +1,8 @@
 use std::fs::{File, OpenOptions};
-use std::io::{BufReader, Read};
+use std::io::{Read, Write, Seek};
 use std::path::Path;
 use serde::{Serialize, Deserialize};
-use std::env;
+
 
 #[derive(Debug, Serialize, Deserialize)]
 struct IncomeOrExpense {
@@ -14,27 +14,18 @@ struct IncomeOrExpense {
 }
 
 pub fn setup() -> Result<(), Box<dyn std::error::Error>> {
+    let file_path = format!("/home/chrisks/finances.json");
 
-    let home: String = env::var("HOME").unwrap();
-    let file_path: String = format!("{}/finances.json", &home);
-
-    let var_path: String = match env::var("FINANCES_PATH") {
-        Ok(v) => v,
-        Err(_) => {
-            match Path::new(&file_path).exists() {
-                true => {
-                    env::set_var("FINANCES_PATH", file_path);
-                    format!("{}/finances.json", &home)
-                }
-                false => {
-                    match File::create(&file_path) {
-                        Ok(_) => {
-                            env::set_var("FINANCES_PATH", file_path);
-                            format!("{}/finances.json",&home)
-                        },
-                        Err(e) => panic!("Failed to create file: {}", e),
-                    }
-                }
+    match Path::new(&file_path).exists() {
+        true => {
+            format!("{}", &file_path)
+        }
+        false => {
+            match File::create(&file_path) {
+                Ok(_) => {
+                    format!("{}",&file_path)
+                },
+                Err(e) => panic!("Failed to create file: {}", e),
             }
         }
     };
@@ -42,23 +33,107 @@ pub fn setup() -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-pub fn add(args: &[String]) -> Result<(), Box<dyn std::error::Error>> {
+pub fn add(args: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
     if args.is_empty() {
         panic!("No arguments provided");
     }
 
-    let var_path: String = env::var("FINANCES_PATH").expect("Failed to get FINANCES_PATH, make sure to run $financers setup");
+    if args.len() < 4 {
+        panic!("Expected 4 arguments: Command, Name, Amount and Type");
+    }
+
+    let file_path = "/home/chrisks/finances.json";
 
     let mut file = OpenOptions::new()
         .read(true)
         .write(true)
         .create(true)
-        .open(var_path)
+        .append(true)
+        .open(file_path)
         .unwrap();
+
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let mut income_or_expense: Vec<IncomeOrExpense> = serde_json::from_str(&contents).unwrap();
+    let mut income_or_expense = match serde_json::from_str::<Vec<IncomeOrExpense>>(&contents) {
+            Ok(v) => v,
+            Err(e) => {
+                if !contents.is_empty() {
+                    return Err(format!(
+                            "Error deserializing file contents: {:?}",
+                            e.to_string()
+                            )
+                        .into());
+                } else {
+                    vec![]
+                }
+            }
+        };
 
-    Ok(())
+    let mut last_id = 0;
+
+    if income_or_expense.is_empty() {
+        print!("{}",1);
+
+        let amount_from_args = args[2].clone();
+        let amount = if let Ok(val) = amount_from_args.parse::<f32>() {
+            val
+        } else {
+            panic!("Invalid amount provided: {}", amount_from_args);
+        };
+
+        let new_income_or_expense = IncomeOrExpense {
+            id: last_id + 1,
+            name: args[1].clone(),
+            amount,
+            type_of: args[3].clone()
+        };
+
+        let mut income_or_expense: Vec<IncomeOrExpense> = Vec::new();
+
+        income_or_expense.push(new_income_or_expense);
+
+        let income_or_expense_json = serde_json::to_string(&income_or_expense)?;
+
+        // file.write_all(b"")?;
+        file.write_all(income_or_expense_json.as_bytes())?;
+
+        Ok(())
+    } else {
+        print!("{}",2);
+
+
+
+        if income_or_expense.len() > 1 {
+            if let Some(last) = income_or_expense.last_mut() {
+                last_id = last.id;
+            };
+        };
+
+
+        let amount_from_args = args[2].clone();
+        let amount = if let Ok(val) = amount_from_args.parse::<f32>() {
+            val
+        } else {
+            panic!("Invalid amount provided: {}", amount_from_args);
+        };
+
+        let new_income_or_expense = IncomeOrExpense {
+            id: last_id + 1,
+            name: args[1].clone(),
+            amount,
+            type_of: args[3].clone()
+        };
+
+        income_or_expense.push(new_income_or_expense);
+
+        let income_or_expense_json = serde_json::to_string(&income_or_expense)?;
+
+        file.set_len(0)?;
+        file.seek(std::io::SeekFrom::Start(0))?;
+        file.write_all(income_or_expense_json.as_bytes())?;
+
+        Ok(())
+    }
+
 }
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
 mod finances;
 use std::env;
 
-use crate::finances::setup;
+use crate::finances::{setup, add};
 
-fn main() {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().skip(1).collect();
 
-    let y = finances::setup();
+    match args.get(0).map(|s| s.as_str()) {
+        Some("setup") => setup().map_err(|e| format!("Error setting up: {}", e))?,
+        // on this case arguments must be: Command, Name, Amount and Type
+        Some("add") => add(args).map_err(|e| format!("Error adding to file: {}", e))?,
+        Some(s) => panic!("Unknown command: {}", s),
+        None => todo!(),
+    };
 
-    todo!()
+    Ok(())
 }
