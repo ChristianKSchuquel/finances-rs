5f4cacbb42636a296b66eb56729e7ff04ff979ff
Christian Schuquel <christianks@protonmail.com>
2023-04-12 14:52:53 -0300

    Updated "add" function and added "remove" function

--- src/finances.rs
+++ src/finances.rs
@@ -1,48 +1,22 @@
-use std::fs::{File, OpenOptions};
+use std::fs::OpenOptions;
 use std::io::{Read, Write, Seek};
-use std::path::Path;
 use serde::{Serialize, Deserialize};
-
 
 #[derive(Debug, Serialize, Deserialize)]
 struct IncomeOrExpense {
-    id: i32,
     name: String,
     amount: f32,
-    // should be either income or expense
-    type_of: String
 }
 
-pub fn setup() -> Result<(), Box<dyn std::error::Error>> {
-    let file_path = format!("/home/chrisks/finances.json");
-
-    match Path::new(&file_path).exists() {
-        true => {
-            format!("{}", &file_path)
-        }
-        false => {
-            match File::create(&file_path) {
-                Ok(_) => {
-                    format!("{}",&file_path)
-                },
-                Err(e) => panic!("Failed to create file: {}", e),
-            }
-        }
-    };
-    
-    Ok(())
-}
-
-pub fn add(args: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+pub fn add(args: Vec<String>, file_path: String) -> Result<(), Box<dyn std::error::Error>> {
     if args.is_empty() {
         panic!("No arguments provided");
     }
 
-    if args.len() < 4 {
-        panic!("Expected 4 arguments: Command, Name, Amount and Type");
+    if args.len() < 3 || args.len() > 3 {
+        panic!("Expected 3 arguments: Command, Name and Amount");
     }
 
-    let file_path = "/home/chrisks/finances.json";
 
     let mut file = OpenOptions::new()
         .read(true)
@@ -56,84 +30,96 @@ pub fn add(args: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
     file.read_to_string(&mut contents)?;
 
     let mut income_or_expense = match serde_json::from_str::<Vec<IncomeOrExpense>>(&contents) {
-            Ok(v) => v,
-            Err(e) => {
-                if !contents.is_empty() {
-                    return Err(format!(
-                            "Error deserializing file contents: {:?}",
-                            e.to_string()
-                            )
-                        .into());
-                } else {
-                    vec![]
-                }
+        Ok(v) => v,
+        Err(e) => {
+            if !contents.is_empty() {
+                return Err(format!(
+                        "Error deserializing file contents: {:?}",
+                        e.to_string()
+                        )
+                    .into());
+            } else {
+                vec![]
             }
-        };
+        }
+    };
 
-    let mut last_id = 0;
-
-    if income_or_expense.is_empty() {
-        print!("{}",1);
-
-        let amount_from_args = args[2].clone();
-        let amount = if let Ok(val) = amount_from_args.parse::<f32>() {
-            val
-        } else {
-            panic!("Invalid amount provided: {}", amount_from_args);
-        };
-
-        let new_income_or_expense = IncomeOrExpense {
-            id: last_id + 1,
-            name: args[1].clone(),
-            amount,
-            type_of: args[3].clone()
-        };
-
-        let mut income_or_expense: Vec<IncomeOrExpense> = Vec::new();
-
-        income_or_expense.push(new_income_or_expense);
-
-        let income_or_expense_json = serde_json::to_string(&income_or_expense)?;
-
-        // file.write_all(b"")?;
-        file.write_all(income_or_expense_json.as_bytes())?;
-
-        Ok(())
+    let amount_from_args = args[2].clone();
+    let amount = if let Ok(val) = amount_from_args.parse::<f32>() {
+        val
     } else {
-        print!("{}",2);
+        panic!("Invalid amount provided: {}", amount_from_args);
+    };
 
+    let new_income_or_expense = IncomeOrExpense {
+        name: args[1].clone(),
+        amount,
+    };
 
+    income_or_expense.push(new_income_or_expense);
 
-        if income_or_expense.len() > 1 {
-            if let Some(last) = income_or_expense.last_mut() {
-                last_id = last.id;
-            };
-        };
+    let income_or_expense_json = serde_json::to_string(&income_or_expense)?;
 
+    file.set_len(0)?;
+    file.seek(std::io::SeekFrom::Start(0))?;
+    file.write_all(income_or_expense_json.as_bytes())?;
 
-        let amount_from_args = args[2].clone();
-        let amount = if let Ok(val) = amount_from_args.parse::<f32>() {
-            val
-        } else {
-            panic!("Invalid amount provided: {}", amount_from_args);
-        };
+    Ok(())
+}
 
-        let new_income_or_expense = IncomeOrExpense {
-            id: last_id + 1,
-            name: args[1].clone(),
-            amount,
-            type_of: args[3].clone()
-        };
-
-        income_or_expense.push(new_income_or_expense);
-
-        let income_or_expense_json = serde_json::to_string(&income_or_expense)?;
-
-        file.set_len(0)?;
-        file.seek(std::io::SeekFrom::Start(0))?;
-        file.write_all(income_or_expense_json.as_bytes())?;
-
-        Ok(())
+pub fn remove(args: Vec<String>, file_path: String) -> Result<(), Box<dyn std::error::Error>> {
+    if args.is_empty() {
+        panic!("No arguments provided");
     }
 
+    if args.len() < 2 || args.len() > 2 {
+        panic!("Expected index");
+    }
+
+
+    let mut file = OpenOptions::new()
+        .read(true)
+        .write(true)
+        .create(true)
+        .append(true)
+        .open(file_path)
+        .unwrap();
+
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+
+    let mut income_or_expense = match serde_json::from_str::<Vec<IncomeOrExpense>>(&contents) {
+        Ok(v) => v,
+        Err(e) => {
+            if !contents.is_empty() {
+                return Err(format!(
+                        "Error deserializing file contents: {:?}",
+                        e.to_string()
+                        )
+                    .into());
+            } else {
+                vec![]
+            }
+        }
+    };
+    
+    let mut index = args[1].parse::<usize>().unwrap();
+
+    println!("{}", income_or_expense.len());
+
+    if income_or_expense.is_empty() || income_or_expense.len() < index {
+        panic!("Invalid id provided");
+    };
+
+    index = index - 1;
+
+    income_or_expense.remove(index);
+
+    let income_or_expense_json = serde_json::to_string(&income_or_expense)?;
+
+    file.set_len(0)?;
+    file.seek(std::io::SeekFrom::Start(0))?;
+    file.write_all(income_or_expense_json.as_bytes())?;
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,18 @@
 mod finances;
 use std::env;
 
-use crate::finances::{setup, add};
+use crate::finances::{add, remove};
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().skip(1).collect();
 
+    let home_dir = dirs::home_dir().unwrap();
+    let file_path = format!("{}/.finances.json", home_dir.display());
+
     match args.get(0).map(|s| s.as_str()) {
-        Some("setup") => setup().map_err(|e| format!("Error setting up: {}", e))?,
-        // on this case arguments must be: Command, Name, Amount and Type
-        Some("add") => add(args).map_err(|e| format!("Error adding to file: {}", e))?,
+        // on this case arguments must be: Command, Name, Amount
+        Some("add") => add(args, file_path).map_err(|e| format!("Error adding to file: {}", e))?,
+        Some("rm") => remove(args, file_path).map_err(|e| format!("Error removing from file: {}", e))?,
         Some(s) => panic!("Unknown command: {}", s),
         None => todo!(),
     };
